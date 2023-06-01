dcc0ad7f8d91aa0ef6cfb84e337c6467e2ae0ee2
Christian Valeri <mortlaxris@outlook.com>
2023-06-01 18:22:53 -0500

    tried add parameters to fetch. Failed

--- src/weather.js
+++ src/weather.js
@@ -4,12 +4,25 @@ function weatherApp(){
     button.addEventListener("click", function click(){
         const inputCity = document.getElementById("input-city-name").value;
         console.log(inputCity);
-        
-        getWeather(inputCity);
+        getLocation();
         clearText();
     })
 
-    async function getWeather(city){
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else { 
+            // x.innerHTML = "Geolocation is not supported by this browser.";
+            console.log("Error: Geolocation is not supported by this browser.")
+        }	
+    }
+    function showPosition(position) {
+        getWeather(position.coords.latitude, position.coords.longitude);
+    }
+
+    async function getWeather(latitude,longitude) {
+        // const addCoords = `https://api.weather.gov/points/${latitude},${longitude}`;
+        // const data = await fetch(addTude);
         const data = await fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast');
         const result = await data.json();
         console.log(result.properties.periods[1].shortForecast);
